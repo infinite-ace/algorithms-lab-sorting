ec0bf65b9b7409eb9fb2ef9ac96cfe0fb684ca0c
infinite-ace <angelkukushev@infinitelambda.com>
2021-11-25 16:11:25 +0200

    removes target and idea

--- src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
+++ src/main/java/com/tap/algos/InsertionSort/InsertionSortMain.java
@@ -2,4 +2,13 @@
 package com.tap.algos.InsertionSort;
 
 public class InsertionSortMain {
+
+    public static int[] insertionSort(int[] arrayToSort) {
+
+        return arrayToSort;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- target/generated-sources/annotations/com/tap/algos/jmh_generated/BenchmarkAlgos_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.tap.algos.jmh_generated;
-public class BenchmarkAlgos_jmhType extends BenchmarkAlgos_jmhType_B3 {
-}
-
